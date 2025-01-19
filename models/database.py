3b1b3563e250089ba3ba98c5912d9ed771fe3863
Rokalia <ksi_ts@mail.ru>
2025-01-19 15:35:03 +0300

    database.py was created

---
+++ models/database.py
@@ -0,0 +1,90 @@
+from typing import Any, List, Dict
+from sqlalchemy import MetaData, text, select
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
+
+db = SQLAlchemy()
+metadata = MetaData()
+
+def initialize(app, database_name:str):
+    uri = 'mysql+pymysql://root:@localhost/'+database_name
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+def get_database():
+    return db
+
+def get_scalars(app, statement):
+    with app.app_context():
+        Session = sessionmaker(bind=db.engine)
+        session = Session()
+        try:
+            res = session.scalars(statement).all()
+        except Exception as e:
+            res = []
+        finally:
+            session.close()
+    return res
+
+def get_scalar(app, statement):
+    with app.app_context():
+        Session = sessionmaker(bind=db.engine)
+        session = Session()
+        try:
+            res = session.scalars(statement).one_or_none()
+        except Exception as e:
+            res = None
+        finally:
+            session.close()
+    return res
+
+def add_item(app, item):
+    with app.app_context():
+        Session = sessionmaker(bind=db.engine)
+        session = Session()
+        try:
+            session.add(item)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            print (f"Error inserting {item.__repr__()}: {str(e)}")
+        finally:
+            session.close()
+
+def add_items(app, items:List[Any]):
+    with app.app_context():
+        Session = sessionmaker(bind=db.engine)
+        session = Session()
+        try:
+            session.add_all(items)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            print (f"Error inserting items: {str(e)}")
+        finally:
+            session.close()
+
+def create_custom_table(app, table_name:str, column_params:List[Dict[str, Any]]):
+    pass
+    # with app.app_context():
+    #     columns = [
+    #         db.Column("id", db.Integer, primary_key=True, autoincrement=True),  # ID столбец
+    #     ]
+
+    #     for element in column_params:
+    #         column_name = element["name"]
+    #         column_type = db.Text if element.get("multiple", False) else db.String(255)
+    #         columns.append(db.Column(column_name, column_type))
+        
+    #     table = db.Table(table_name, metadata, *columns, extend_existing=True)
+    #     metadata.create_all(bind=db.engine)
+    # return table
+
+def fill_custom_table(app, table_name:str, results:List[Dict[str, str]]):
+    pass
+    # with app.app_context():
+    #     table = db.Table(table_name, metadata, autoload_with=db.engine)
+        
